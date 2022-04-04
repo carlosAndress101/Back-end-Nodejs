4342ff2f5b714382578435604a22d8779ca54b5e
carlos.hinestrozap <https://github.com/CarlosAndress/Project_Frontend.git>
2022-04-04 18:22:04 -0500

    commit2

---
+++ src/helpers/auth.js
@@ -0,0 +1,12 @@
+// import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcryptjs';
+
+// const auth = {
+//   login: () =>{
+//     console.log('Hola mundo');
+//   }
+// }
+// export default auth;
+
+
+
