1463eb917dcd65c5315493a89f0cec9ba55b4395
carlos.hinestrozap <https://github.com/CarlosAndress/Project_Frontend.git>
2022-05-05 21:48:55 -0500

    Admin-model-graphql

--- src/model/Admin/Admin.js
+++ src/model/Admin/Admin.js
@@ -2,40 +2,40 @@ import mongoose from "mongoose";
 
 const Admin = mongoose.Schema(
     {
-        Name:{
-            type:String,
-            required:true,
+        Name: {
+            type: String,
+            required: true,
             trim: true,
         },
-        Lastname:{
-            type:String,
-            required:true,
-            trim:true,
-        },
-        Age:{
-            type:Number,
-            required:true,
-        },
-        Phonenumber:{
-            type:Number,
-            required:true,
-        },
-        Email:{
-            type:String,
+        Lastname: {
+            type: String,
             required: true,
-            trim:true,
+            trim: true,
         },
-        password:{
-            type:String,
-            required:true,
-            trim:true,
+        Age: {
+            type: Number,
+            required: true,
         },
-        city:{
-            type:String,
-            required,
+        Phonenumber: {
+            type: Number,
+            required: true,
+        },
+        Email: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        city: {
+            type: String,
+            required: true,
         },
     },
-    {timetamps: true},
+    { timetamps: true },
 );
 
 export default mongoose.model("admin", Admin);
