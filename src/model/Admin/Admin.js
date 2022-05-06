9cde75edc4e29c0d2fed8a8277c93256230c4248
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-05-06 02:01:30 +0200

    admin-run

---
+++ src/model/Admin/Admin.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const Admin = mongoose.Schema(
+    {
+        Name:{
+            type:String,
+            required:true,
+            trim: true,
+        },
+        Lastname:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        Age:{
+            type:Number,
+            required:true,
+        },
+        Phonenumber:{
+            type:Number,
+            required:true,
+        },
+        Email:{
+            type:String,
+            required: true,
+            trim:true,
+        },
+        password:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        city:{
+            type:String,
+            required,
+        },
+    },
+    {timetamps: true},
+);
+
+export default mongoose.model("admin", Admin);
