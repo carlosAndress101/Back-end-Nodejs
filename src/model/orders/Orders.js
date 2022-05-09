dc21b6c3bfb52857ab441625517066fc836d0578
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-05-09 17:41:29 +0200

    changed order

--- src/model/orders/Orders.js
+++ src/model/orders/Orders.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Orders = mongoose.Schema(
     {
         NumberOfClient: {
-            type: Number,
+            type:String,
             required: true,
         },
         dataOfOrder: {
